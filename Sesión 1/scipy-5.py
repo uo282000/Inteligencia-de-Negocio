28f7567d1a69957881918fc3542844cf29a556a6
Pablo Fernández <uo282000@uniovi.es>
2026-09-23 18:56:20 +0200

    script5 terminado

--- Sesión 1/scipy-5.py
+++ Sesión 1/scipy-5.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.spatial.distance import minkowski
+from scipy.spatial.distance import minkowski, chebyshev
 
 rejilla = np.meshgrid(
     np.linspace(-1.1, 1.1, 512),
@@ -16,6 +16,21 @@ def bola(p):
 
 plt.imshow(
     bola(3),
+    extent=[-1.1, 1.1, -1.1, 1.1],
+    origin="lower",
+    cmap="Blues",
+)
+plt.gca().set_aspect("equal")
+plt.axis("off")
+plt.show()
+
+def en_bola_chebyshev(x, y):
+    return chebyshev([x, y], [0, 0]) <= 1
+def bola_chebyshev():
+    return np.vectorize(en_bola_chebyshev)(X, Y)
+
+plt.imshow(
+    bola_chebyshev(),
     extent=[-1.1, 1.1, -1.1, 1.1],
     origin="lower",
     cmap="Blues",
